661ce3bcb4d32baeaeccd0c6dff27a37401b586e
Dan Walkes <danwalkes@trellis-logic.com>
2020-05-10 10:11:35 -0600

    Add User LED GPIO Using Cube MX

--- Inc/main.h
+++ Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define UserLED_Pin GPIO_PIN_13
+#define UserLED_GPIO_Port GPIOC
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
